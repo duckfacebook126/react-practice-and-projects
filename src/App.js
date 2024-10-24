2972b5fcbcdac25f16cbda73ef5b1d663e3caf60
duckfacebook126 <duckfacebook126@gmail.com>
2024-10-24 17:43:25 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from "./COmponents/Navbar"
+import MainComp from "./COmponents/Main"
+ function Application()
+{
+
+return(<>
+<MainComp/>
+<Navbar/>
+</>
+ );
+} 
+
+export default Application;
